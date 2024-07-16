efb94db3f0d06bbb756ba1821e99ec1a10ee8eb0
Atiq Mansoori <er_asm@outlook.com>
2024-07-16 15:30:23 +0530

    Utility module added

--- test.py
+++ test.py
@@ -1,4 +1,8 @@
 from src import logging
+from src.utils.common import read_yaml
+
 
 if __name__=="__main__":
     logging.info("Asslamu alaikum")
+    read_yaml('H:\mask detector\Face-Mask-Detector\params.yaml')
+    
