0f171359e9ed00b9ebf490ae11b5779a596e87f9
Atiq Mansoori <er_asm@outlook.com>
2024-07-17 11:14:41 +0530

    Base Model Prepared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from src.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-# from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from src.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 # from src.pipeline.stage_03_training import ModelTrainingPipeline
 # from src.pipeline.stage_04_evaluation import EvaluationPipeline
 from src import logger
@@ -17,16 +17,16 @@ except Exception as e:
         raise e
 
 
-# STAGE_NAME = "Prepare base model"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 # STAGE_NAME = "Training"
